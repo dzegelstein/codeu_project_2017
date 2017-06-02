3939876da55d829aeb007bfe2cc718922c32d54d
Sherry <sherrybai01@gmail.com>
2017-06-02 08:51:54 -0400

    added javadoc comments to PasswordReader.java

--- src/codeu/chat/client/commandline/PasswordReader.java
+++ src/codeu/chat/client/commandline/PasswordReader.java
@@ -2,6 +2,13 @@ package codeu.chat.client.commandline;
 
 import java.io.Console;
 
+/**
+* The PasswordReader class uses the readPassword function in
+* java.io.Console to mask the password input as it is being entered
+* in the command line.
+*
+* @author Sherry Bai
+*/
 public final class PasswordReader {
   private Console console;
 
@@ -9,6 +16,12 @@ public final class PasswordReader {
     console = null;
   }
 
+  /**
+  * This method attempts to print a prompt to the current terminal
+  * and read a password input to that terminal.
+  * @param prompt This is the prompt printed to the terminal.
+  * @return String This is the password read in by the readPassword function.
+  */
   public String read(String prompt) {
       String password = null;
       try {
